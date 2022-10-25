c6d4d5f1e36c83a18aa6a635a1293586ca68347f
adr-gabriel <111366554+adr-gabriel@users.noreply.github.com>
2022-10-25 10:54:18 -0300

    Create principal.js

---
+++ principal.js
@@ -0,0 +1,15 @@
+introducao-javascript
+├── css
+│   ├── index.css
+│   └── reset.css
+├── js
+│   └── principal.js
+├── img
+├── favicon.ico
+└── index.html// Restante do código HTML
+            </section>
+        </main>
+        <!-- Importação dos Javascripts AQUI -->
+        <script src="js/principal.js"></script>
+    </body>
+</html><h1>adrian</h1><h1 class="titulo">adrian</h1>
