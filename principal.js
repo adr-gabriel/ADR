c05014c31f9092f70e4e8222086460520408af79
adr-gabriel <111366554+adr-gabriel@users.noreply.github.com>
2022-11-08 10:57:06 -0300

    Create principal.js

---
+++ principal.js
@@ -0,0 +1,21 @@
+introducao-javascript
+├── css
+│   ├── index.css
+│   └── reset.css
+├── js
+│   └── principal.js
+├── img
+├── favicon.ico
+└── index.html
+// Restante do código HTML
+            </section>
+        </main>
+        <!-- Importação dos Javascripts AQUI -->
+        <script src="js/principal.js"></script>
+    </body>
+</html><h1>adrian nutricao</h1>
+  <h1 class="titulo">adrian nutricao</h1>
+document.querySelector(".titulo");
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "adrian nutricao"
+<!-- restante do HTML -->
